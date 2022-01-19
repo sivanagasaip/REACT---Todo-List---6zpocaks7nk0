39b414a77f36a7d156368155fbc3fa79149afb61
kartikdhomne <92445043+kartikdhomne@users.noreply.github.com>
2022-01-19 23:42:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,66 @@
-import React from "react";
-import "./../styles/App.css";
+import React, {useState} from "react";
+import "./App.css";
 
 function App() 
 {
+	const [tasks,setTasks]=useState([]);
+    const [text,setText]=useState("");
+    const [si,setSi]=useState(-1);
+	const [edit,setEdit]=useState("");
+	
+	const addTask = (title) =>{
+		if(title!=""){
+		const newT=[...tasks,{title}];
+        setTasks(newT);
+        setText("");
+		}
+        
+    };
+    const deleteTask = (index) =>{
+        const newT=[...tasks];
+        newT.splice(index,1);
+        setTasks(newT)
+	};
+	
+	const changeTask = (event) =>{
+		setText(event.target.value);
+	}
+	const submitTask = (event) =>{
+		// event.preventDefault();
+		if(!edit) return;
+		const newT=[...tasks];
+		newT[si].title=edit;
+		setTasks(newT);
+        setEdit("");
+        setText("");
+		setSi(-1);
+	};
+   
 	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+		<div id="main">
+		<textarea type="text" id="task" onChange={changeTask}  placeholder="Add a new task..."></textarea>
+		<button type="button" id="btn" onClick={()=>addTask(text)}>Add</button>
+		<ol>
+		{
+			tasks.map((task,index)=>(
+				<li key={index}>
+					<>
+                        <span className="list">{task.title}</span>
+					<button className="edit" onClick={()=>{setSi(index)}}>Edit</button>					
+					<button id={index} className="delete" onClick={()=>deleteTask(index)}>Delete</button>
+                    </>
+					{
+						(si===index)?
+						<>
+							<textarea className="editTask" type="text" id="editedTask" onChange={(evt)=>{setEdit(evt.target.value)}} ></textarea>
+							<button className="saveTask" type="button" id="submit" onClick={submitTask}>Submit</button>
+						</>
+						:(null)
+					}
+				</li>
+			))
+		}
+		</ol>
 	</div>
 	);
 }
